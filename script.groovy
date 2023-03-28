df565e6c80fab666be094b47264973cfb0444523
aiishaa <57088227+aiishaa@users.noreply.github.com>
2023-03-28 09:16:56 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,12 +4,11 @@ def buildImage() {
     withCredentials([usernamePassword(credentialsId: 'dockerHubRepo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t aishafathy/demo-app:jma-2.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push aishafathy/demo-app:jma-2.0'
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    sh 'docker push aishafathy/demo-app:jma-2.0'
 } 
 
 return this
